75fd2b84fae539ecbcf69891057377fa82d51916
dv14jln <dv14jln@cs.umu.se>
2016-12-05 15:58:06 +0100

    Modded Monday

--- schedTest.c
+++ schedTest.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 #include "workerProg.h"
 
 /*struct sched_param {
@@ -19,6 +20,35 @@ void *dummyfunc(void *empty){
 }
 
 int main ( int argc , char *argv[] ) {
+	// To gather options of program. 
+	int op=0;
+	while((op =	getopt(argc,argv,"t:n:")) != -1) {
+		switch(op) {
+			case 't':
+				printf("t %s\n", optarg);
+			//	printf("%s\n",optarg);
+				//tamp=optarg;
+			//	option=tamp[0]; 
+				break;
+			case 'n':
+				printf("n %s\n", optarg);
+			//	printf("%s\n",optarg );
+				//threadCount=atoi(optarg);
+				//frsInd=optind;
+				break;
+			case ':':
+		//		printf("%s, %c\n", optarg,optopt);
+				break;
+
+			case '?':
+		//		printf("%s, %c\n", optarg,optopt);
+				break;
+
+			default:
+				printf("DEFAULT %s\n", optarg);
+				break;
+		}
+	}
 	//http://man7.org/linux/man-pages/man2/sched_setscheduler.2.html
 	// int sched_setscheduler(pid_t pid, int policy,
     //                          const struct sched_param *param);
@@ -30,10 +60,13 @@ int main ( int argc , char *argv[] ) {
 //	sched_setscheduler(0,SCHED_OTHER,param);
 //	sched_setscheduler(0,SCHED_FIFO,param);
 //	sched_setscheduler(0,SCHED_RR,param);
-	//sched_setscheduler(0,0,param);
+	sched_setscheduler(0,0,param);
 	int i = sched_getscheduler(0);
 	printf("Current scheduler is %d\n"	,i);	
 	struct stuff *blo = malloc(sizeof(struct stuff));
+	struct evaluatorStat *evla= malloc(sizeof(struct evaluatorStat));
+	evla->clock= 10;
+	fprintf(stdout, "clock = %d\n",evla->clock );
 	void* p;
 	doWork(blo);
 	printf("\nafter doWork\n");
--- workerProg.h
+++ workerProg.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
 
 struct stuff
 {
@@ -11,6 +12,13 @@ long workClocker;
 int scheduler;
 } stuff;
 
+struct evaluatorStat {
+long clock ;   // Keeps time from first operation to last after running the testpointer.
+void *funcPtr; // Points to the test that is actually being run.
+int NrOfWork; // should be variable, can't only try one work size.
+long CPUCycles; // If we can actually count how many cpuCycles program uses, lets write it to this.
+bool testComplete; // Could the test actually be run/completed ? Best to make sure.
+} evaluator;
 
 
 void* doWork(void *workStruct)  ;
